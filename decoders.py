e7321affadeb858717fb4fb4bc4a565e08f4f746
Guitchounts <gg42@bu.edu>
2017-09-19 14:48:32 -0400

    lstm

--- decoders.py
+++ decoders.py
@@ -596,7 +596,7 @@ class LSTMDecoder(object):
 
         model=Sequential() #Declare model
         #Add recurrent layer
-        model.add(GRU(self.units,activation='tanh',input_shape=(X_train.shape[1],X_train.shape[2]),dropout_W=self.dropout,dropout_U=self.dropout)) #Within recurrent layer, include dropout
+        model.add(LSTM(self.units,activation='tanh',input_shape=(X_train.shape[1],X_train.shape[2]),dropout_W=self.dropout,dropout_U=self.dropout)) #Within recurrent layer, include dropout
         if self.dropout!=0: model.add(Dropout(self.dropout)) #Dropout some units (recurrent layer output units)
         # recurrent_activation='hard_sigmoid'
         #model.add(Dense(10,activation='tanh')) #return_sequences=True
