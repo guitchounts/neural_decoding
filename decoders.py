7f42bd24a0037dd3c8c7e4bc7bccf969d13dfbaa
Guitchounts <gg42@bu.edu>
2017-09-18 23:22:27 -0400

    relu

--- decoders.py
+++ decoders.py
@@ -567,7 +567,7 @@ class LSTMDecoder(object):
         Whether to show progress of the fit after each epoch
     """
 
-    def __init__(self,units=800,dropout=0,num_epochs=1000,verbose=1):
+    def __init__(self,units=400,dropout=0,num_epochs=1000,verbose=1):
          self.units=units
          self.dropout=dropout
          self.num_epochs=num_epochs
@@ -591,10 +591,13 @@ class LSTMDecoder(object):
 
         model=Sequential() #Declare model
         #Add recurrent layer
-        model.add(LSTM(self.units,input_shape=(X_train.shape[1],X_train.shape[2]),dropout_W=self.dropout,dropout_U=self.dropout)) #Within recurrent layer, include dropout
+        model.add(LSTM(self.units,activation='relu',input_shape=(X_train.shape[1],X_train.shape[2]),dropout_W=self.dropout,dropout_U=self.dropout)) #Within recurrent layer, include dropout
         if self.dropout!=0: model.add(Dropout(self.dropout)) #Dropout some units (recurrent layer output units)
 
         model.add(Dense(64,activation = 'tanh'))
+
+        #model.add(Dense(2,activation = 'relu'))
+
 
         #Add dense connections to output layer
         model.add(Dense(y_train.shape[1]))
